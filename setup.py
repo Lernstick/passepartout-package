a79c8ba02a47836c1385a31e949ed490747dd788
Gaudenz Steinlin <gaudenz@soziologie.ch>
2016-07-12 16:41:58 +0200

    Add templates to python package
    Add the debian package control file templates to the Python packageing
    as additional data files.

--- setup.py
+++ setup.py
@@ -48,6 +48,13 @@ setup(
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
     packages=['passepartout'],
+    package_data={'passepartout': ['templates/compat',
+                                   'templates/control',
+                                   'templates/menu.desktop',
+                                   'templates/menu.directory',
+                                   'templates/merge.menu',
+                                   'templates/rules',
+                                   'templates/source/*']},
 
     # List run-time dependencies here.  These will be installed by pip when your
     # project is installed. For an analysis of "install_requires" vs pip's
