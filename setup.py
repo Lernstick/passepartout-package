138b37b41b0f7f02c86cf54b8c5ab399d73adca4
Gaudenz Steinlin <gaudenz@soziologie.ch>
2016-07-12 16:40:36 +0200

    Fix packageing
    The py_modules parameter (no longer) works, packages is what's really
    intended.

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
-    py_modules=['passepartout'],
+    packages=['passepartout'],
 
     # List run-time dependencies here.  These will be installed by pip when your
     # project is installed. For an analysis of "install_requires" vs pip's
